a31295fec5d6e7f9154d1f881ab27e6517fc445b
Jbern16 <jbern16@gmail.com>
2016-07-15 20:06:23 -0600

    tests pass

--- test/test.js
+++ test/test.js
@@ -5,11 +5,11 @@ describe('Votes', function(){
     describe('voteObject can be added to', function(){
         it('should be instantiated with empty object', function(){
             var votes = app.votes;
-            assert.equal(votes, {});
+            assert.deepEqual(votes, {});
         });
         it('should be incremented when vote is cast', function(){
             var count = app.countVotes({'1': 'C'});
-            assert.equal(count, { A: 0, B: 0, C: 1, D: 0 });
+            assert.deepEqual(count, { A: 0, B: 0, C: 1, D: 0 });
         });
     });
 });
